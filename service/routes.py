43dae8cb4465e839e5285f593182ad4934528b3f
thalhathta <thalhathta24@gmail.com>
2025-08-04 09:56:31 -0400

    Implemented CRUD methods

--- service/routes.py
+++ service/routes.py
@@ -116,6 +116,19 @@ def update_account(account_id):
 ######################################################################
 
 # ... place you code here to DELETE an account ...
+@app.route("/accounts/<int:account_id>", methods =["DELETE"])
+def delete_accounts(account_id):
+    """
+        Delete an Account
+    """
+    app.logger.info("Request to delete an Account with id: %s", account_id)
+    
+    account = Account.find(account_id)
+    if account:
+        account.delete()
+
+    return '',status.HTTP_204_NO_CONTENT 
+
 
 
 ######################################################################
