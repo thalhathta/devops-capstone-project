7a5e40cf3f64f06794e5f11888be5bbc9c6c48b1
thalhathta <thalhathta24@gmail.com>
2025-08-04 07:11:24 -0400

    added List accounts

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -139,3 +139,12 @@ class TestAccountService(TestCase):
         response = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(response.status_code,status.HTTP_404_NOT_FOUND)
         
+    def test_list_all_accounts(self):
+        """It should List all Accounts"""
+        self._create_accounts(3)
+        response = self.client.get(BASE_URL)
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(len(data),3)
+
+
